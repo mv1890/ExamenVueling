9bfaf7548fb6fb30180d50a54282d86f34cb4cc4
mvergara <marc1890@gmail.com>
2019-03-06 18:40:36 +0100

    Modelos añadidos

---
+++ ExamenVueling/Data/Repository/Models/RateModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repository.Models
+{
+    public class RateModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Rate { get; set; }
+    }
+}
